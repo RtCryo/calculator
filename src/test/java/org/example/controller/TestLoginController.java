1088ac7d8894390485488e2c6b563aea9c6b3db6
RTC <mikhail.bei@web.de>
2021-11-04 19:05:16 +0100

    Controllers testing

--- src/test/java/org/example/controller/TestLoginController.java
+++ src/test/java/org/example/controller/TestLoginController.java
@@ -1,19 +1,24 @@
 package org.example.controller;
 
+import org.example.config.SecurityConfig;
+import org.example.service.MyUserDetailsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = LoginController.class)
-@ContextConfiguration(classes = {TestLoginController.Config.class, WithMockCustomUserSecurityContextFactory.class})
+@ContextConfiguration(classes = {TestLoginController.Config.class, WithMockCustomUserSecurityContextFactory.class, SecurityConfig.class, LoginController.class})
 public class TestLoginController {
 
     @Autowired
@@ -27,13 +32,17 @@ public class TestLoginController {
     }
 
     @Test
-    public void getLoginControllerAuthenticated() throws Exception {
-        this.mockMvc.perform(get("/login"))
-                .andExpect(status().is3xxRedirection());
+    public void postLoginControllerUnauthenticated() throws Exception {
+        this.mockMvc.perform(formLogin("/login").user("bob").password("111"))
+                .andExpect(status().isOk());
     }
 
     @Configuration
     static class Config{
+
+        @MockBean
+        @Qualifier("myUserDetailsService")
+        public MyUserDetailsService myUserDetailsService;
 
     }
 }
